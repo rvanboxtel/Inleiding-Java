3e60b230500801b3f3b3d08caebd22fd65082149
rvanboxtel <rvanboxtel@roc-dev.com>
2016-09-23 12:58:29 +0200

    Rejected commit: Default

--- Hoofdstukken/src/HoofdstukZes/Praktijk.java
+++ Hoofdstukken/src/HoofdstukZes/Praktijk.java
@@ -13,16 +13,16 @@ public class Praktijk extends Applet {
     double cijferTwee;
     double cijferDrie;
     double totaalCijfer;
-    double gemiddeld;
+    int gemiddeld;
     double gemiddeldEind;
 
     public void init() {
         cijferEen = 5.9;
         cijferTwee = 6.3;
         cijferDrie = 6.9;
-        totaalCijfer = cijferEen + cijferTwee + cijferDrie;
-        gemiddeld = totaalCijfer / 3;
-        gemiddeldEind = Math.round(( gemiddeld) * 10) / 10.0;
+        totaalCijfer = (cijferEen + cijferTwee + cijferDrie) / 3;
+        gemiddeld = (int) (totaalCijfer * 10);
+        gemiddeldEind = (double) gemiddeld / 10;
     }
 
     public void paint(Graphics g){
